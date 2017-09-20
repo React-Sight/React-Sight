d3242bde305ac3964f9adde6ee86af5cf52ce5a3
William <he.william@yahoo.com>
2017-09-20 16:21:44 -0700

    added d3 zoom and drag feature on d3 tree

--- connection.js
+++ connection.js
@@ -44,14 +44,14 @@ function drawChart(treeData) {
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-  .attr("transform", "translate("
-  + margin.left + "," + margin.top + ")")
+    .attr("transform", "translate("
+    + margin.left + "," + margin.top + ")")
 
   d3.select("body")
     .call(d3.zoom().on("zoom", function () {
       svg.attr("transform", d3.event.transform)
     })
-    .scaleExtent([1, 8]))
+      .scaleExtent([1, 8]))
 
 
 
