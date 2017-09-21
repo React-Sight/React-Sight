bb5fc6d1dbf5c7770b48924ce529ca1b68effe70
David Sally <davidcsally@gmail.com>
2017-09-21 13:13:50 -0700

    no changes, git error

--- connection.js
+++ connection.js
@@ -1,33 +1,33 @@
 function createChannel() {
-  console.log('connected to CHANNEL...')
+  console.log('Creating connection...')
   var port = chrome.extension.connect({
     name: "connecting to VisualizeIO"
   });
 
+  console.log('# port.onMessage.addListener')  
   port.onMessage.addListener(function (data) {
-    console.log('GOT THE DATA', data)
+    // console.log('GOT THE DATA', data)
     drawChart(data.data[0].children[0])
   })
 };
 
 function sendObjectToInspectedPage(message) {
-  console.log('sending something !!!!')
+  // console.log('sending something !!!!')
   message.tabId = chrome.devtools.inspectedWindow.tabId;
+  console.log('# chrome.extension.sendMessage')  
+  console.log(message)  
   chrome.extension.sendMessage(message)
 }
 
 // attach panel to chrome dev tools
 chrome.devtools.panels.create("dataViz", null, "devtools.html", function () {
+  console.log('# chrome.devtools.panels.create')
   createChannel()
-  console.log('in the callback');
+  console.log('# sendObjectToInspectedPage')  
   sendObjectToInspectedPage({ action: "script", content: "messageback-script.js" })
+  sendObjectToInspectedPage({action: "script", content: "inserted-script.js"});
+  
 });
-
-function sendObjectToDevTools(message) {
-  chrome.extension.sendMessage(message, function (message) {
-    console.log('HELLO YOU SENT A MESSAGE BACK: ', message)
-  })
-}
 
 function drawChart(treeData) {
   var margin = { top: 50, right: 50, bottom: 50, left: 50 },
