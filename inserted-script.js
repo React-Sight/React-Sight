2fde896fd7237b12cea9cc4d894eb9712fab34bc
David Sally <davidcsally@gmail.com>
2017-09-21 13:22:36 -0700

    removed old files

--- inserted-script.js
+++
@@ -1,7 +0,0 @@
-// This is included and executed in the inspected page
-function inserted() {
-	console.log('External script attached');
-	var event = new Event('attached');
-	window.dispatchEvent(event);		
-}
-inserted();
