84db178ef38cd6f701df6605b373b2d9ff5c679e
David Sally <davidcsally@gmail.com>
2017-09-22 17:18:11 -0700

    added filters to remove redux or router components from tree

---
+++ chrome-ext/filters.js
@@ -0,0 +1,34 @@
+/** Parse JSON recursively and remove unwanted items */
+const recurFilter = (node, parentArr, filter) => {
+  if (node.name == undefined) return
+  const newObj = { name: node.name, children: [] }
+  if (filter.includes(node.name)) {
+    node.children.forEach(child => {
+      recurFilter(child, parentArr, filter);
+    });
+  }
+  else {
+    parentArr.push(newObj);
+    node.children.forEach(child => {
+      recurFilter(child, newObj.children, filter);
+    });
+  }
+}
+
+/** Removes Redux components from tree */
+export function filterRedux(data) {
+  const filtered = { data: [] }
+  const names = ['Provider', 'Connect'];
+
+  recurFilter(data.data[0], filtered.data, names)
+  return filtered;
+}
+
+/** Removes ReactRouter v4 components from JSON */
+export function filterRouter(data) {
+  const filtered = { data: [] }
+  const names = ['BrowserRouter', 'Router', 'Switch', 'Route'];
+
+  recurFilter(data.data[0], filtered.data, names)
+  return filtered;
+}
