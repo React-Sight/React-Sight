d70d8872988c5792374262bd14964fc67a3e4e3a
Grant Kang <g1k05j@gmail.com>
2017-09-28 10:30:34 -0700

    breadcrumb nav bp

--- chrome-ext/store-panel.js
+++ chrome-ext/store-panel.js
@@ -7,7 +7,7 @@ const drawStore = (data) => {
   store.innerHTML = ''
   reduxStore.forEach(state => {
     console.log('redux state:', state)
-    const newValue = new JSONFormatter(data[state], 1, {hoverPreviewEnabled: true})
+    const newValue = new JSONFormatter(data[state], 0, {hoverPreviewEnabled: true, hoverPreviewArrayCount: 10})
     const newItem = document.createElement('li')
     const newState = document.createElement('p')
     newState.innerHTML = state
---
+++ chrome-ext/vbox.js
@@ -0,0 +1,8 @@
+// const breadcrumb: HTMLElement = document.querySelector('.breadcrumb');
+// const breadcrumbSteps: HTMLElement = document.querySelectorAll('.breadcrumb__step');
+
+// const breadcrumb = document.getElementById("vbox")
+// const vboxCrumbs = document.querySelectorAll('.breadcrumb__step')
+const drawVBox = (data) => {
+
+}
