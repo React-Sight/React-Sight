bf5cc963ac91d019f995f85f0684d48eafa20a50
David Sally <davidcsally@gmail.com>
2017-10-10 10:36:38 -0700

    better fiber support

--- chrome-ext/backend/installHook.js
+++ chrome-ext/backend/installHook.js
@@ -1,105 +1,73 @@
 //might need additional testing..renderers provides a list of all imported React instances
-console.log(window)
 const reactInstances = window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers || null;
-
-console.log('react dev tools: ', window.__REACT_DEVTOOLS_GLOBAL_HOOK__)
-//grab the first instance of imported React library
 const instance = reactInstances[Object.keys(reactInstances)[0]];
-// console.log('[0]: ', reactInstances[Object.keys(reactInstances)[0]]);
-// console.log('[1]: ', reactInstances[Object.keys(reactInstances)[1]]);
-
-console.log('instance: ', instance);
-console.log(instance.findFiberByHostInstance);
-
-const reactRoot = window.document.body.childNodes
-console.log('')
+const reactRoot = window.document.body.childNodes;
 const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+//grab the first instance of imported React library
+console.log('#__REACT_DEVTOOLS_GLOBAL_HOOK__: ', window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
+console.log('#__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers[0]: ', instance);
 
 var rootNode;
 var throttle = false;
+var fiberDOM;
+var version;
+
 // var store = [];
 //locate instance of __REACT_DEVTOOLS_GLOBAL_HOOK__
 //__REACT_DEVTOOLS_GLOBAL_HOOK__ exists if React is imported in inspected Window
 
-var vd;
 
 (function installHook() {
   //no instance of React
 
-
   if (!window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
     //should run loading animation here probably
-    return console.log('Cannot find React library...')
+    return console.log('Cannot find React library...');
   }
 
   if (instance && instance.version === '16.0.0') {
-
-
-    console.log('devTools.onCommitFiberRoot', devTools.onCommitFiberRoot)
+    version = 16;
     devTools.onCommitFiberRoot = (function (original) {
-      console.log('onCommitFiberRoot')
       return function (...args) {
-        console.log('inside onCommitFiberRoot', args)
-        vd = args[1]
-        traverse16()
-        return original(...args)
+        fiberDOM = args[1];
+        traverse16();
+        return original(...args);
       }
-    })(devTools.onCommitFiberRoot)
-
-
-    // console.log('getfiber roots', devTools.getFiberRoots)
-    // devTools.getFiberRoots = (function (original) {
-    //   console.log('hello')
-    //   return function (...args) {
-    //     console.log(args);
-    //     console.log(args[0]);
-    //     rootNode = args[0];
-    //     if (!throttle) {
-    //       throttle = true
-    //       setTimeout(() => {
-    //         // instance.findFiberByHostInstance(rootNode)
-    //         // instance.findHostInstanceByFiber(rootNode)
-    //         // devTools.onCommitFiberRoot(rootNode, rootNode)
-    //         traverse16()
-    //         throttle = false
-    //       }, 10)
-    //     }
-    //     return original(...args)
-    //   }
-    // })(devTools.getFiberRoots)
+    })(devTools.onCommitFiberRoot);
   }
   else {
     //hijack receiveComponent method which runs after a component is rendered
     instance.Reconciler.receiveComponent = (function (original) {
       return function (...args) {
         if (!throttle) {
-          throttle = true
+          throttle = true;
           setTimeout(() => {
             getData();
-            throttle = false
+            throttle = false;
           }, 10)
         }
-        return original(...args)
+        return original(...args);
       }
-    })(instance.Reconciler.receiveComponent)
+    })(instance.Reconciler.receiveComponent);
   }
 })();
 
 const getData = (components = [], store = []) => {
-  console.log('#getData')
+  // console.log('#getData')
   //define rootElement of virtual DOM
   const rootElement = instance.Mount._instancesByReactRootID[1]._renderedComponent;
   // console.log('rootElement: ', rootElement)
   //recursively traverse down through props chain   starting from root element
   traverseAllChildren(rootElement, components);
   const data = { data: components, store: store }
-  console.log('retrieved data --> posting to content-scripts...: ', data)
+  // console.log('retrieved data --> posting to content-scripts...: ', data)
   window.postMessage(JSON.parse(JSON.stringify(data)), '*');
 };
 
 const traverseAllChildren = (component, parentArr) => {
   // if no current element, return
-  if (!component._currentElement) return
+  if (!component._currentElement) return;
 
   // console.log('current component: ', component)
 
@@ -116,44 +84,44 @@ const traverseAllChildren = (component, parentArr) => {
 
   // Get ID -> DO NOT REMOVE
   if (component._domID) {
-    newComponent.id = component._domID
+    newComponent.id = component._domID;
     newComponent.isDOM = true;
   }
   else {
-    newComponent.id = component._mountOrder * 100
+    newComponent.id = component._mountOrder * 100;
     newComponent.isDOM = false;
   }
 
   // Get type
   if (!newComponent.type && component.constructor && component.constructor.name) {
-    newComponent.type = component.constructor.name
+    newComponent.type = component.constructor.name;
   }
 
   // Get Name
   if (component._currentElement.type) {
     // check for displayName or name
-    if (component._currentElement.type.displayName) newComponent.name = component._currentElement.type.displayName
-    else if (component._currentElement.type.name) newComponent.name = component._currentElement.type.name
-    else newComponent.name = component._currentElement.type
+    if (component._currentElement.type.displayName) newComponent.name = component._currentElement.type.displayName;
+    else if (component._currentElement.type.name) newComponent.name = component._currentElement.type.name;
+    else newComponent.name = component._currentElement.type;
   }
-  else newComponent.name = 'default'
+  else newComponent.name = 'default';
 
   // Get State
   if (!newComponent.state && component._instance && component._instance.state) {
-    newComponent.state = component._instance.state
+    newComponent.state = component._instance.state;
     if (newComponent.state && Object.keys(newComponent.state).length === 0) {
       newComponent.state = null;
     }
   }
   if (!newComponent.props && component._currentElement && component._currentElement.props) {
-    newComponent.props = parseProps(component._currentElement.props)
+    newComponent.props = parseProps(component._currentElement.props);
   }
   //Get key
   if (!newComponent.key && component._currentElement && component._currentElement.key) {
-    newComponent.key = component._currentElement.key
+    newComponent.key = component._currentElement.key;
   }
   if (!newComponent.ref && component._currentElement && component._currentElement.ref) {
-    newComponent.ref = component._currentElement.ref
+    newComponent.ref = component._currentElement.ref;
   }
 
   //go into children of current component
@@ -162,11 +130,11 @@ const traverseAllChildren = (component, parentArr) => {
   if (componentChildren) {
     const keys = Object.keys(componentChildren)
     keys.forEach(key => {
-      traverseAllChildren(componentChildren[key], newComponent.children)
+      traverseAllChildren(componentChildren[key], newComponent.children);
     })
   }
   else if (component._renderedComponent) {
-    traverseAllChildren(component._renderedComponent, newComponent.children)
+    traverseAllChildren(component._renderedComponent, newComponent.children);
   }
 }
 
@@ -209,21 +177,35 @@ const parseProps = (props) => {
 
 // listener for initial load
 window.addEventListener('reactsight', e => {
-  if (version = 16) traverse16()
-  else getData()
-})
+  if (version = 16) traverse16();
+  else getData();
+});
 
 
+/**
+ * Traversal Method for React 16
+ * 
+ * If the application is using React Fiber, run this method to crawl the virtual DOM.
+ * First, find the React mount point, then walk through each node
+ * For each node, grab the state and props if present
+ * 
+ * */
+function traverse16(components = []) {
+  // console.log('#traverse16 vDOM: ', fiberDOM);
+  recur16(fiberDOM.current.stateNode.current, components);
+  let data = { data: components };
+  data.data = data.data[0].children;
+  console.log('retrieved data --> posting to content-scripts...: ', data)
+  window.postMessage(JSON.parse(JSON.stringify(data)), '*');
+}
 
-/** TODO: Get State & Props
- *
+/** TODO: Get Props
  *
  * Traverse through vDOM (React 16) and build up JSON data
  *
  */
 function recur16(node, parentArr) {
-  // console.log('#recur16', node)
-  console.log('ID', node._debugID)
+  console.log('#recur16', node);
   const newComponent = {
     name: '',
     children: [],
@@ -231,71 +213,62 @@ function recur16(node, parentArr) {
     props: null,
     id: null,
     isDOM: null,
-  }
+  };
 
+  // TODO ** only works on local host **
   // get ID
   if (node._debugID) newComponent.id = node._debugID;
 
-  // get name
+  // get name and type
   if (node.type) {
     if (node.type.name) {
-      newComponent.name = node.type.name
-      newComponent.isDOM = false
+      newComponent.name = node.type.name;
+      newComponent.isDOM = false;
     }
     else {
-      newComponent.name = node.type
-      newComponent.isDOM = true
+      newComponent.name = node.type;
+      newComponent.isDOM = true;
     }
   }
 
   // get state
-  if (node.memoizedState) newComponent.state = node.memoizedState
+  if (node.memoizedState) newComponent.state = node.memoizedState;
 
   // get props
-  // if (node.memoizedProps) newComponent.props = this.props16(node)
+  if (node.memoizedProps) newComponent.props = props16(node)
 
-  newComponent.children = []
-  parentArr.push(newComponent)
-  if (node.child != null) this.recur16(node.child, newComponent.children)
-  if (node.sibling != null) this.recur16(node.sibling, parentArr)
+  newComponent.children = [];
+  parentArr.push(newComponent);
+  if (node.child != null) recur16(node.child, newComponent.children);
+  if (node.sibling != null) recur16(node.sibling, parentArr);
 }
+
 /** TODO - get objects to work
  *
  * Parse the props for React 16 components
  */
-props16 = node => {
-  const props = {}
-  const keys = Object.keys(node.memoizedProps)
+function props16(node) {
+  const props = {};
+  const keys = Object.keys(node.memoizedProps);
+
   keys.forEach(prop => {
     if (typeof node.memoizedProps[prop] === 'function') {
-      props[prop] = '' + node.memoizedProps[prop]
+      props[prop] = '' + node.memoizedProps[prop];
     }
     else if (typeof node.memoizedProps[prop] === 'object') {
-      props[prop] = 'object*'
+      props[prop] = 'object*';
     }
     else if (prop === 'children') {
-      props[prop] = new node.memoizedProps[prop].constructor
+      props[prop] = new node.memoizedProps[prop].constructor;
       if (Array.isArray(node.memoizedProps[prop])) {
         node.memoizedProps[prop].forEach(child => {
-          props[prop].push(child && child.type && child.type.name)
+          props[prop].push(child && child.type && child.type.name);
         })
       }
-      else props[prop].name = node.memoizedProps[prop].type && node.memoizedProps[prop].type.name
+      else props[prop].name = node.memoizedProps[prop].type && node.memoizedProps[prop].type.name;
     }
 
-    else props[prop] = node.memoizedProps[prop]
-  })
+    else props[prop] = node.memoizedProps[prop];
+  });
   return props;
-}
-
-/** Traversal Method for React 16 */
-function traverse16(components = []) {
-  // const vDOM = devTools._fiberRoots[rootNode];
-  console.log('#traverse16 vDOM: ', vd);
-  recur16(vd.current.stateNode.current, components)
-  let data = { data: components }
-  console.log('retrieved data --> posting to content-scripts...: ', data)
-
-  data.data = data.data[0].children
-  window.postMessage(JSON.parse(JSON.stringify(data)), '*')
 }
