19087439dee23f05b49826bd9acd30c5e64ed6a1
David Sally <davidcsally@gmail.com>
2017-10-10 11:31:00 -0700

    added semi colons

--- chrome-ext/frontend/store-panel.js
+++ chrome-ext/frontend/store-panel.js
@@ -2,17 +2,17 @@ import JSONFormatter from 'json-formatter-js';
 
 const drawStore = (data) => {
   if (Object.keys(data).length) {
-    const storeNode = document.getElementById('store')
-    storeNode.innerHTML = ''
+    const storeNode = document.getElementById('store');
+    storeNode.innerHTML = '';
     const storeFormatter = new JSONFormatter(data, 1, {
       hoverPreviewEnabled: false,
       hoverPreviewArrayCount: 10,
       hoverPreviewFieldCount: 10,
       animateOpen: true,
       animateClose: true,
-    })
-    storeNode.appendChild(storeFormatter.render())
+    });
+    storeNode.appendChild(storeFormatter.render());
   }
-}
+};
 
 export default drawStore;
