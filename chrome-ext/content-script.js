1888f7723bcaaf692134fff204adb580322fad0e
David Sally <davidcsally@gmail.com>
2017-10-10 17:15:18 -0700

    prevent tree from trying to draw strings

--- chrome-ext/content-script.js
+++ chrome-ext/content-script.js
@@ -3,7 +3,9 @@ window.addEventListener('message', e => {
   if (e.source !== window) return
   //send message to background
   chrome.extension.sendMessage(e.data, function () {
-    console.log('**Content-scripts** received data sending to devtools...', e.data)
+    if (typeof e.data === 'object') {
+      console.log('**Content-scripts** received data sending to devtools...', e.data)
+    }
   })
 });
 
@@ -17,7 +19,7 @@ chrome.extension.onMessage.addListener((message, sender) => {
   listener is currently emitting anytime a new tab is open */
 })
 
-injectScript( chrome.extension.getURL('/backend/installHook.js'), 'body');
+injectScript(chrome.extension.getURL('/backend/installHook.js'), 'body');
 /** function to inject traversal script into running tab's context */
 function injectScript(file, node) {
   var th = document.getElementsByTagName(node)[0];
