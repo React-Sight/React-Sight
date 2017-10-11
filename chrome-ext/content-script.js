52610adae0a7278c7d7c1029c6b784d518d3c6d3
David Sally <davidcsally@gmail.com>
2017-10-10 19:08:46 -0700

    linted

--- chrome-ext/content-script.js
+++ chrome-ext/content-script.js
@@ -1,30 +1,31 @@
-//Listening for events emitted from user's application *window.postMessage()*
-window.addEventListener('message', e => {
-  if (e.source !== window) return
-  //send message to background
-  chrome.extension.sendMessage(e.data, function () {
+/** function to inject traversal script into running tab's context */
+function injectScript(file, node) {
+  const th = document.getElementsByTagName(node)[0];
+  const s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', file);
+  th.appendChild(s);
+}
+
+// Listening for events emitted from user's application *window.postMessage()*
+window.addEventListener('message', (e) => {
+  if (e.source !== window) return;
+  // send message to background
+  chrome.extension.sendMessage(e.data, () => {
     if (typeof e.data === 'object') {
-      console.log('**Content-scripts** received data sending to devtools...', e.data)
+      console.log('**Content-scripts** received data sending to devtools...', e.data);
     }
-  })
+  });
 });
 
-chrome.extension.onMessage.addListener((message, sender) => {
-  var newEvent = new Event('reactsight')
-  window.dispatchEvent(newEvent)
+chrome.extension.onMessage.addListener(() => {
+  const newEvent = new Event('reactsight');
+  window.dispatchEvent(newEvent);
   // panelLoaded = true
   /** DEVELOPER NOTES:
   additional testing required with panelLoaded...message handler
   should only emit event when user clicks on React-Sight panel..
   listener is currently emitting anytime a new tab is open */
-})
+});
 
 injectScript(chrome.extension.getURL('/backend/installHook.js'), 'body');
-/** function to inject traversal script into running tab's context */
-function injectScript(file, node) {
-  var th = document.getElementsByTagName(node)[0];
-  var s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', file);
-  th.appendChild(s);
-}
