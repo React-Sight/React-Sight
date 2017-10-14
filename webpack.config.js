a260e2e0610579b5d42a5e254953766b125a2445
David Sally <davidcsally@gmail.com>
2017-10-14 16:10:32 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const WebpackMonitor = require('webpack-monitor');
 
 module.exports = {
   entry: './chrome-ext/frontend/devtools.js',
