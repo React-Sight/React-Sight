6333db1e1a1c6bba18650e7e230d0f4435639790
David Sally <davidcsally@gmail.com>
2017-10-31 20:49:37 -0700

    documented functions

--- chrome-ext/frontend/breadcrumb.js
+++ chrome-ext/frontend/breadcrumb.js
@@ -11,17 +11,22 @@
  * @param {object} object - function will build up an object of names and the number of times they appear
  */
 const getNodeNames = (data, object) => {
+  // base cases
   if (!data.name) return;
-  if (!data.isDOM) {
-    object[data.name] = (object[data.name] || 0) + 1;
-  }
   if (!data.children.length) return;
+  
+  // first instance of node
+  if (!data.isDOM) object[data.name] = (object[data.name] || 0) + 1;
 
-  data.children.forEach((child) => {
-    getNodeNames(child, object);
-  });
+  // iterate through children
+  data.children.forEach(child => getNodeNames(child, object));
 };
 
+/**
+ * Clears the current breadcrumb and appends an updated version
+ * 
+ * @param {object} data - representation of React's vDOM
+ */
 const drawVBox = (data) => {
   let anchorText;
   const nodeNames = {};
@@ -29,16 +34,21 @@ const drawVBox = (data) => {
   const nodes = Object.keys(nodeNames);
   const breadcrumbSteps = [];
 
+  // iterate through list of components and create DOM nodes
+  // store DOM nodes in breadcrumbSteps 
   nodes.forEach((node) => {
     const anchor = document.createElement('a');
     anchor.setAttribute('class', 'breadcrumb-item');
     anchor.setAttribute('href', '#');
 
+    // set number of times node appears
     if (nodeNames[node] === 1) anchorText = document.createTextNode(`${node}`);
     else anchorText = document.createTextNode(`${node}[${nodeNames[node]}]`);
     anchor.appendChild(anchorText);
     breadcrumbSteps.push(anchor);
   });
+
+  // select current breadcrumb, clear it, and re-append new breadcrumb by iteratring through breadcrumbSteps
   const breadcrumb = document.querySelector('.breadcrumb');
   breadcrumb.innerHTML = '';
   breadcrumbSteps.forEach((node) => {
