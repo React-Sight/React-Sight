f164aa81b81e15f6eb9e098cf7c934e24f906ad3
David Sally <davidcsally@gmail.com>
2017-10-31 21:00:58 -0700

    documented functions

--- chrome-ext/frontend/loader.js
+++ chrome-ext/frontend/loader.js
@@ -1,6 +1,7 @@
 //  Created by Grant Kang, William He, and David Sally on 9/10/17.
 //  Copyright © 2017 React Sight. All rights reserved.
 
+// Array of funny loading messages
 const loaderHeaders = [
   'Centering divs...',
   'Calibrating the DOM...',
@@ -18,8 +19,15 @@ const loaderHeaders = [
   'Initializing content-scripts...',
 ];
 
+/**
+ * Appends the loading screen with a random quote.
+ * 
+ * The loader is removed when / if the tree is drawn, after data has been recieved from the inspected page
+ * 
+ * After 10 seconds, it will show an error, indicating it could not find React's root node
+ */
 function processLoader() {
-  // console.log('...processing loader');
+  // select loader and change message in text node
   const header = document.getElementById('loader-header');
   header.innerHTML = loaderHeaders[Math.floor(Math.random() * loaderHeaders.length)];
 
