5475cd73438414f4c006ea0a6a9f79bdde57c0c6
David Sally <davidcsally@gmail.com>
2017-11-28 17:56:58 -0800

    fixed?

--- test/index.js
+++ test/index.js
@@ -1,4 +1,10 @@
 /* eslint-env mocha */
-describe('test', () => {
+var assert = require('assert');
 
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
 });
