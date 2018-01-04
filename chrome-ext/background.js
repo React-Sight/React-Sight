60f0a4cab9407ea3c63cace40a1293761ce519c2
David Sally <davidcsally@gmail.com>
2018-01-03 22:59:03 -0800

    little linting changes

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,44 +1,47 @@
 //  Created by Grant Kang, William He, and David Sally on 9/10/17.
 //  Copyright © 2017 React Sight. All rights reserved.
 
-const connections = {}
+/* eslint max-len: off, no-console: off, prefer-arrow-callback: off */
+
+const connections = {};
 
 chrome.extension.onConnect.addListener(function (port) {
   let extensionListener = (message, sender, res) => {
-    //creates a new key/value pair of current window & devtools tab when a new devtools tab is opened
+    // creates a new key/value pair of current window & devtools tab when a new devtools tab is opened
     if (message.name == 'connect' && message.tabId) {
-      //sends a message to content-scripts when a new tab is opened to instantiate the tree
-      chrome.tabs.sendMessage(message.tabId, message)
+      // sends a message to content-scripts when a new tab is opened to instantiate the tree
+      chrome.tabs.sendMessage(message.tabId, message);
       connections[message.tabId] = port;
       return;
     }
-    //other message handling
-  }
+    // other message handling
+  };
   // Listens to messages sent from devtools
   port.onMessage.addListener(extensionListener);
   port.onDisconnect.addListener(function (port) {
-    port.onMessage.removeListener(extensionListener)
+    port.onMessage.removeListener(extensionListener);
 
-    let tabs = Object.keys(connections)
+    let tabs = Object.keys(connections);
     for (let i = 0; i < tabs.length; i++) {
       if (connections[tabs[i]] == port) {
-        delete connections[tabs[i]]
+        delete connections[tabs[i]];
         break;
       }
     }
-  })
-})
-//Receives message from content-scripts and checks for valid connections before posting to devtools
+  });
+});
+
+// Receives message from content-scripts and checks for valid connections before posting to devtools
 chrome.extension.onMessage.addListener(function (req, sender, res) {
   if (sender.tab) {
-    let tabId = sender.tab.id
+    let tabId = sender.tab.id;
     if (tabId in connections) {
-      connections[tabId].postMessage(req)
+      connections[tabId].postMessage(req);
     } else {
-      console.log('WARNING:: Tab not found in connection list')
+      console.log('WARNING:: Tab not found in connection list');
     }
   } else {
-    console.log("WARNING:: sender.tab not defined")
+    console.log('WARNING:: sender.tab not defined');
   }
-  return true
-})
+  return true;
+});
