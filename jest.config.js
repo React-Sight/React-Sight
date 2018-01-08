49a168f2397bef0672960662cf48875b471fb3a4
David Sally <davidcsally@gmail.com>
2018-01-07 17:23:18 -0800

    Add store-panel, ignore bundle

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,13 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['**/chrome-ext/**/*.js'],
   coveragePathIgnorePatterns: [
+    '<rootDir>/chrome-ext/build/bundle.js',
     '<rootDir>/chrome-ext/asset/',
     '<rootDir>/chrome-ext/backend/installHook.js',
     '<rootDir>/chrome-ext/background.js',
     '<rootDir>/chrome-ext/content-script.js',
     '<rootDir>/chrome-ext/frontend/devtools.js',
     '<rootDir>/chrome-ext/frontend/drawChart.js',
-    '<rootDir>/chrome-ext/frontend/store-panel.js',
     '<rootDir>/chrome-ext/frontend/breadcrumb.js',
   ],
 };
