2259ea0f1319a1b9b0dc35185090f0d5dee3e9c6
David Sally <davidcsally@gmail.com>
2018-01-07 18:31:50 -0800

    Test suite for store-panel function

---
+++ chrome-ext/frontend/store-panel.spec.js
@@ -0,0 +1,34 @@
+//  Created by Grant Kang, William He, and David Sally on 9/10/17.
+//  Copyright © 2018 React Sight. All rights reserved.
+
+/* eslint-env jest */
+import drawStore from './store-panel';
+import {
+  mockReduxStore,
+  mockStorePanel,
+  mockJSONFormatter,
+  mockJSONFormatterExpanded,
+} from '../../test/fixtures';
+
+describe('drawStore', () => {
+  it('should not run if no mockReduxStore is passed', () => {
+    const result = drawStore({});
+    expect(result).toEqual(null);
+  });
+
+  it('should render a JSONFormatter element if data is present', () => {
+    document.body.innerHTML = mockStorePanel;
+    drawStore(mockReduxStore);
+    const storeNode = document.getElementById('store').innerHTML;
+    expect(storeNode).toEqual(mockJSONFormatter);
+  });
+
+  it('should open the panel if it was previously open', () => {
+    document.body.innerHTML = mockStorePanel;
+    let previousStore = drawStore(mockReduxStore);
+    previousStore._isOpen = 1;
+    previousStore = drawStore(mockReduxStore, previousStore);
+    const storeNode = document.getElementById('store').innerHTML;
+    expect(storeNode).toEqual(mockJSONFormatterExpanded);
+  });
+});
